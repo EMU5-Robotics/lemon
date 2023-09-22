929be6351770869e69fbfd5cb5c14c4971225020
nonl4331 <nicholasl0003@gmail.com>
2023-09-22 21:31:42 +1000

    slight cleanup + rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use client::coprocessor::serial::{find_v5_port, Serial, SerialData};
-use protocol::{ControlPkt, StatusPkt, device::ControllerButtons};
+use protocol::{device::ControllerButtons, ControlPkt, StatusPkt};
 use std::time::Duration;
 
 mod replay;
@@ -13,8 +13,8 @@ fn main() {
 	let serial = Serial::open(&ports.0.port_name).unwrap();
 	let data = serial.spawn_threaded();
 
-	#[allow(unused_assignments)]
-	let (mut last, mut current) = initialise(&data);
+	let mut last = get_input(&data);
+	let mut current;
 
 	let drive = Drive::new(
 		[
@@ -30,46 +30,50 @@ fn main() {
 		0.8,
 	);
 
-    let mut recorder = Recorder::new();
-    let mut player = Player::from_file("test.replay").unwrap();
+	let mut recorder = Recorder::new();
+	let mut player = Player::from_file("test.replay").unwrap_or_default();
 
-    let mut axes = [0, 0, 0, 0];
+	let mut axes = [0, 0, 0, 0];
 
 	loop {
 		current = get_input(&data);
 		let mut output = get_last_output(&data);
 
-        let mut input_changes = InputChanges::from_pair(&last, &current);
+		let mut input_changes = InputChanges::from_pair(&last, &current);
 
-        // check for player independent events
-        if input_changes.released(ControllerButtons::A) {
-            recorder.toggle().unwrap();
-        } else if input_changes.pressed(ControllerButtons::A) && recorder != Recorder::Off {
-            recorder.toggle().unwrap();
-        }
+		// check for player independent events
+		if input_changes.released(ControllerButtons::A)
+			|| (input_changes.pressed(ControllerButtons::A) && recorder != Recorder::Off)
+		{
+			if let Err(e) = recorder.toggle() {
+				log::error!("recorder toggle failed with: {e}");
+			}
+		}
 
-        if input_changes.released(ControllerButtons::B) {
-            player = player.play();
-        }
+		if input_changes.released(ControllerButtons::B) {
+			player = player.play();
+		}
 
-        recorder.take_event(&input_changes).unwrap();
+		if let Err(e) = recorder.take_event(&input_changes) {
+			log::error!("recorder failed to take event with: {e}");
+		}
 
-        if player.is_playing() {
-            // play replay
-            let events = player.get_events();
-            if !events.is_empty() {
-                // override input changes event
-                input_changes = events[0].1;
-            }
-        }
+		if player.is_playing() {
+			// play replay
+			let events = player.get_events();
+			if !events.is_empty() {
+				// override input changes event
+				input_changes = events[0].1;
+			}
+		}
 
-        // update axes if detected change
-        if let Some(new_axes) = input_changes.axes {
-            axes = new_axes;
-        }
+		// update axes if detected change
+		if let Some(new_axes) = input_changes.axes {
+			axes = new_axes;
+		}
 
 		let d_power = axes[1] as f32 / 127.0;
-	    let t_power = axes[2] as f32 / 127.0;
+		let t_power = axes[2] as f32 / 127.0;
 		drive.drive(d_power, t_power, &mut output);
 
 		send_data(&data, output);
@@ -80,37 +84,46 @@ fn main() {
 
 #[derive(Debug, PartialEq, Copy, Clone)]
 pub struct InputChanges {
-    pressed: ControllerButtons,
-    released: ControllerButtons,
-    axes: Option<[i8; 4]>,
+	pressed: ControllerButtons,
+	released: ControllerButtons,
+	axes: Option<[i8; 4]>,
 }
 
 impl InputChanges {
-    pub const NOCHANGE: Self = InputChanges { pressed: ControllerButtons::empty(), released: ControllerButtons::empty(), axes: None};
+	pub const NOCHANGE: Self = InputChanges {
+		pressed: ControllerButtons::empty(),
+		released: ControllerButtons::empty(),
+		axes: None,
+	};
 
-    pub fn from_pair(last: &StatusPkt, current: &StatusPkt) -> Self {
-        let pressed = current.controller_buttons & !last.controller_buttons;
+	pub fn from_pair(last: &StatusPkt, current: &StatusPkt) -> Self {
+		let pressed = current.controller_buttons & !last.controller_buttons;
 
-        let released = !current.controller_buttons & last.controller_buttons;
+		let released = !current.controller_buttons & last.controller_buttons;
 
-        let axes = if current.controller_axes != last.controller_axes {
-            Some(current.controller_axes)
-        } else {
-            None
-        };
+		let axes = if current.controller_axes != last.controller_axes {
+			Some(current.controller_axes)
+		} else {
+			None
+		};
 
-        Self { pressed, released, axes }
-    }
-    pub fn changes(&self) -> bool {
-        self.pressed != ControllerButtons::empty() || self.released != ControllerButtons::empty() || self.axes.is_some()
-    }
-    pub fn released(&self, buttons: ControllerButtons) -> bool {
-        self.released & buttons != ControllerButtons::empty()
-    }
-    pub fn pressed(&self, buttons: ControllerButtons) -> bool {
-        self.pressed & buttons != ControllerButtons::empty()
-    }
-    
+		Self {
+			pressed,
+			released,
+			axes,
+		}
+	}
+	pub fn changes(&self) -> bool {
+		self.pressed != ControllerButtons::empty()
+			|| self.released != ControllerButtons::empty()
+			|| self.axes.is_some()
+	}
+	pub fn released(&self, buttons: ControllerButtons) -> bool {
+		self.released & buttons != ControllerButtons::empty()
+	}
+	pub fn pressed(&self, buttons: ControllerButtons) -> bool {
+		self.pressed & buttons != ControllerButtons::empty()
+	}
 }
 
 struct Motor {
@@ -175,21 +188,11 @@ impl Drive {
 }
 
 fn get_input(data: &SerialData) -> StatusPkt {
-	{
-		data.recv_pkt_lock().unwrap().clone()
-	}
+	data.recv_pkt_lock().unwrap().clone()
 }
 
 fn get_last_output(data: &SerialData) -> ControlPkt {
-	{
-		data.send_pkt_lock().unwrap().clone()
-	}
-}
-
-fn initialise(data: &SerialData) -> (StatusPkt, StatusPkt) {
-	let last = { data.recv_pkt_lock().unwrap().clone() };
-	let current = { data.recv_pkt_lock().unwrap().clone() };
-	(last, current)
+	data.send_pkt_lock().unwrap().clone()
 }
 
 fn send_data(data: &SerialData, packet: ControlPkt) {
--- src/replay.rs
+++ src/replay.rs
@@ -1,248 +1,318 @@
-use chrono::prelude::*;
 use crate::InputChanges;
+use chrono::prelude::*;
 use protocol::device::ControllerButtons;
-use std::time::Instant;
-use std::io::{self, Write, BufRead};
-
+use std::io::{self, BufRead, Write};
+use std::{fmt, time::Instant};
 
 #[derive(Debug)]
 pub enum ReplayError {
-   InvalidCodePath, 
-   IoError(io::Error),
-   ParseError(String),
-   MaxTimeExceeded,
+	InvalidCodePath,
+	IoError(io::Error),
+	ParseError(String),
+	MaxTimeExceeded,
 }
 
+impl fmt::Display for ReplayError {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match self {
+			Self::InvalidCodePath => write!(f, "Invalid Code Path"),
+			Self::IoError(e) => write!(f, "{e}"),
+			Self::ParseError(e) => write!(f, "{e}"),
+			Self::MaxTimeExceeded => write!(f, "Max Time Exceeded"),
+		}
+	}
+}
 
 impl From<io::Error> for ReplayError {
-    fn from(err: io::Error) -> Self {
-        ReplayError::IoError(err)
-    }
+	fn from(err: io::Error) -> Self {
+		ReplayError::IoError(err)
+	}
 }
 
 #[derive(Debug, PartialEq)]
 pub enum Recorder {
-    Off,
-    Waiting(Instant),
-    Recording{ last: Instant, events: Vec<(u32, InputChanges)> }
+	Off,
+	Waiting(Instant),
+	Recording {
+		last: Instant,
+		events: Vec<(u32, InputChanges)>,
+	},
 }
 
 impl Recorder {
-    pub fn new() -> Self {
-        Self::Off
-    }
+	pub fn new() -> Self {
+		Self::Off
+	}
 
-    pub fn toggle(&mut self) -> Result<(), ReplayError> {
-        match self {
-            Self::Off => {
-                *self = Self::Waiting(Instant::now());
-            },
-            Self::Waiting(_) => {
-                *self = Self::Off;
-            },
-            Self::Recording{last: _, events} => {
-               Self::write_events(events)?;
-               *self = Self::Off;
-            },
-        }
-        Ok(())
-    }
+	pub fn toggle(&mut self) -> Result<(), ReplayError> {
+		match self {
+			Self::Off => {
+				*self = Self::Waiting(Instant::now());
+			}
+			Self::Waiting(_) => {
+				*self = Self::Off;
+			}
+			Self::Recording { last: _, events } => {
+				Self::write_events(events)?;
+				*self = Self::Off;
+			}
+		}
+		Ok(())
+	}
 
-    pub fn take_event(&mut self, changes: &InputChanges) -> Result<(), ReplayError> {
-        match self {
-            Self::Off => {},
-            Self::Waiting(ref last) | Self::Recording{ref last, ..} => {
-                if !changes.changes() {
-                    return Ok(());
-                }
+	pub fn take_event(&mut self, changes: &InputChanges) -> Result<(), ReplayError> {
+		match self {
+			Self::Off => {}
+			Self::Waiting(ref last) | Self::Recording { ref last, .. } => {
+				if !changes.changes() {
+					return Ok(());
+				}
 
-                let elapsed = last.elapsed().as_micros() / 100;
+				let elapsed = last.elapsed().as_micros() / 100;
 
-                if elapsed > u32::MAX as u128 {
-                    *self = Self::Off;
-                    log::error!("recording exceeded maximum time between events (~119 hours).");
-                    return Err(ReplayError::MaxTimeExceeded);
-                }
+				if elapsed > u32::MAX as u128 {
+					*self = Self::Off;
+					log::error!("recording exceeded maximum time between events (~119 hours).");
+					return Err(ReplayError::MaxTimeExceeded);
+				}
 
-                match self {
-                    Self::Waiting(_) => {
-                        *self = Self::Recording{last: Instant::now(), events: vec![(elapsed as u32, *changes)]};
-                    }
-                    Self::Recording{events, last, ..} => {
-                        *last = Instant::now();
-                        events.push((elapsed as u32, *changes));
-                    }
-                    _ => {
-                        log::error!("invalid state in take_event reached, this is a bug.");
-                        return Err(ReplayError::InvalidCodePath);
-                    },
-                }
-            },
-        }
-        Ok(())
-    }
+				match self {
+					Self::Waiting(_) => {
+						*self = Self::Recording {
+							last: Instant::now(),
+							events: vec![(elapsed as u32, *changes)],
+						};
+					}
+					Self::Recording { events, last, .. } => {
+						*last = Instant::now();
+						events.push((elapsed as u32, *changes));
+					}
+					_ => {
+						log::error!("invalid state in take_event reached, this is a bug.");
+						return Err(ReplayError::InvalidCodePath);
+					}
+				}
+			}
+		}
+		Ok(())
+	}
 
+	fn write_events(events: &[(u32, InputChanges)]) -> Result<(), ReplayError> {
+		let filename = Local::now().format("%Y-%m-%d_%H:%M:%S.replay").to_string();
 
-    fn write_events(events: &[(u32, InputChanges)]) -> Result<(), ReplayError> {
-        let filename = Local::now().format("%Y-%m-%d_%H:%M:%S.replay").to_string();
+		let mut file = std::fs::File::create(filename)?;
 
-        let mut file = std::fs::File::create(filename)?;
-
-        for (diff, changes) in events {
-            write!(file, "{diff},{},{}", changes.pressed.bits(), changes.released.bits())?;
-            if let Some(axes) = changes.axes {
-                write!(file, ",{},{},{},{}\n", axes[0], axes[1], axes[2], axes[3])
-            } else {
-                write!(file, "\n")
-            }?;
-        }
-        Ok(())
-    }
+		for (diff, changes) in events {
+			write!(
+				file,
+				"{diff},{},{}",
+				changes.pressed.bits(),
+				changes.released.bits()
+			)?;
+			if let Some(axes) = changes.axes {
+				write!(file, ",{},{},{},{}\n", axes[0], axes[1], axes[2], axes[3])
+			} else {
+				write!(file, "\n")
+			}?;
+		}
+		Ok(())
+	}
 }
 
 #[derive(Debug)]
 pub enum Player {
-    Off(Vec<(u32, InputChanges)>),
-    Playing{last: Instant, events: Vec<(u32, InputChanges)>, cursor: usize}
+	Off(Vec<(u32, InputChanges)>),
+	Playing {
+		last: Instant,
+		events: Vec<(u32, InputChanges)>,
+		cursor: usize,
+	},
+}
+
+impl Default for Player {
+	fn default() -> Self {
+		Player::Off(Vec::new())
+	}
 }
 
 impl Player {
-    pub fn from_file(filename: &str) -> Result<Self, ReplayError> {
-        let file = std::fs::File::open(filename)?;
+	pub fn from_file(filename: &str) -> Result<Self, ReplayError> {
+		let file = std::fs::File::open(filename)?;
 
-        let mut reader = std::io::BufReader::new(file);
+		let mut reader = std::io::BufReader::new(file);
 
-        let mut events = Vec::new();
+		let mut events = Vec::new();
 
-        let mut line = 1;
-        loop {
-            let mut changes = InputChanges::NOCHANGE;
-            let mut string = String::new();
-            if reader.read_line(&mut string).is_err() { break; }
-            let things = string.trim().split(',').collect::<Vec<_>>();
-            let length = things.len();
-            if string.is_empty() {
-                break;
-            }
+		let mut line = 1;
+		loop {
+			let mut changes = InputChanges::NOCHANGE;
+			let mut string = String::new();
+			if reader.read_line(&mut string).is_err() {
+				break;
+			}
+			let things = string.trim().split(',').collect::<Vec<_>>();
+			let length = things.len();
+			if string.is_empty() {
+				break;
+			}
 
-            if length != 3 && length != 7 {
-                return Err(ReplayError::ParseError(format!("line {line} has an invalid number of items: {length}")));
-            }
+			if length != 3 && length != 7 {
+				return Err(ReplayError::ParseError(format!(
+					"line {line} has an invalid number of items: {length}"
+				)));
+			}
 
-            let Ok(diff_time) = things[0].parse() else {
-                return Err(ReplayError::ParseError(format!("line {line} has an invalid diff time: {}", things[0])));
-            };
+			let Ok(diff_time) = things[0].parse() else {
+				return Err(ReplayError::ParseError(format!(
+					"line {line} has an invalid diff time: {}",
+					things[0]
+				)));
+			};
 
-            let Ok(Some(pressed)) = things[1].parse::<u16>().map(|v| ControllerButtons::from_bits(v)) else {
-                return Err(ReplayError::ParseError(format!("line {line} has invalid pressed bitfield: {}", things[1])))
-            };
-            changes.pressed = pressed;
+			let Ok(Some(pressed)) = things[1]
+				.parse::<u16>()
+				.map(|v| ControllerButtons::from_bits(v))
+			else {
+				return Err(ReplayError::ParseError(format!(
+					"line {line} has invalid pressed bitfield: {}",
+					things[1]
+				)));
+			};
+			changes.pressed = pressed;
 
-            let Ok(Some(released)) = things[2].parse::<u16>().map(|v| ControllerButtons::from_bits(v)) else {
-                return Err(ReplayError::ParseError(format!("line {line} has invalid released bitfield: {}", things[2])))
-            };
-            changes.released = released;
+			let Ok(Some(released)) = things[2]
+				.parse::<u16>()
+				.map(|v| ControllerButtons::from_bits(v))
+			else {
+				return Err(ReplayError::ParseError(format!(
+					"line {line} has invalid released bitfield: {}",
+					things[2]
+				)));
+			};
+			changes.released = released;
 
-            if length == 7 {
-                let Ok(lx) = things[3].parse::<i8>() else {
-                    return Err(ReplayError::ParseError(format!("line {line} has invalid lx value: {}", things[3])));
-                };
-                let Ok(ly) = things[4].parse::<i8>() else {
-                    return Err(ReplayError::ParseError(format!("line {line} has invalid ly value: {}", things[4])));
-                };
-                let Ok(rx) = things[5].parse::<i8>() else {
-                    return Err(ReplayError::ParseError(format!("line {line} has invalid rx value: {}", things[5])));
-                };
-                let Ok(ry) = things[6].parse::<i8>() else {
-                    return Err(ReplayError::ParseError(format!("line {line} has invalid rx value: {}", things[6])));
-                };
-                changes.axes = Some([lx, ly, rx, ry]);
-            }
-            events.push((diff_time, changes));
-            line += 1;
-        }
-        Ok(Self::Off(events))
-    }
+			if length == 7 {
+				let Ok(lx) = things[3].parse::<i8>() else {
+					return Err(ReplayError::ParseError(format!(
+						"line {line} has invalid lx value: {}",
+						things[3]
+					)));
+				};
+				let Ok(ly) = things[4].parse::<i8>() else {
+					return Err(ReplayError::ParseError(format!(
+						"line {line} has invalid ly value: {}",
+						things[4]
+					)));
+				};
+				let Ok(rx) = things[5].parse::<i8>() else {
+					return Err(ReplayError::ParseError(format!(
+						"line {line} has invalid rx value: {}",
+						things[5]
+					)));
+				};
+				let Ok(ry) = things[6].parse::<i8>() else {
+					return Err(ReplayError::ParseError(format!(
+						"line {line} has invalid rx value: {}",
+						things[6]
+					)));
+				};
+				changes.axes = Some([lx, ly, rx, ry]);
+			}
+			events.push((diff_time, changes));
+			line += 1;
+		}
+		Ok(Self::Off(events))
+	}
 
-    pub fn play(self) -> Self {
-        match self {
-            Self::Off(events) => {
-                log::info!("Player started");
-                Self::Playing{last: Instant::now(), events, cursor: 0 }
-            }
-            Self::Playing{..} => {
-                log::warn!("Player::play was called while in state Playing.");
-                self
-            }
-        }
-    }
+	pub fn play(self) -> Self {
+		match self {
+			Self::Off(events) => {
+				log::info!("Player started");
+				Self::Playing {
+					last: Instant::now(),
+					events,
+					cursor: 0,
+				}
+			}
+			Self::Playing { .. } => {
+				log::warn!("Player::play was called while in state Playing.");
+				self
+			}
+		}
+	}
 
-    pub fn get_events(&mut self) -> &[(u32, InputChanges)] {
-        let (reset, range) = match self {
-            Self::Off(_) => {
-                log::warn!("Player::get_events was called while in state Off.");
-                return &[];
-            }
-            Self::Playing{last, events, cursor} => {
-                let start_index = *cursor;
-                let mut event_sum = 0;
-                loop {
-                    // end of playback reached
-                    if *cursor >= events.len() {
-                        log::info!("playback ended!");
-                        if *cursor - start_index > 1 {
-                            log::warn!("more than one event returned! (is robot processing slowly?).");
-                        }
-                        break (true, start_index..*cursor);
-                    }
+	pub fn get_events(&mut self) -> &[(u32, InputChanges)] {
+		let (reset, range) = match self {
+			Self::Off(_) => {
+				log::warn!("Player::get_events was called while in state Off.");
+				return &[];
+			}
+			Self::Playing {
+				last,
+				events,
+				cursor,
+			} => {
+				let start_index = *cursor;
+				let mut event_sum = 0;
+				loop {
+					// end of playback reached
+					if *cursor >= events.len() {
+						log::info!("playback ended!");
+						if *cursor - start_index > 1 {
+							log::warn!(
+								"more than one event returned! (is robot processing slowly?)."
+							);
+						}
+						break (true, start_index..*cursor);
+					}
 
-                    // time since last_update
-                    let elapsed = last.elapsed().as_micros() / 100;
-                    if elapsed > u32::MAX as u128 {
-                        log::error!("recording exceeded maximum time between events (~119 hours), resetting.");
-                        break (true, 0..0);
-                    }
-                    let elapsed = elapsed as u32;
+					// time since last_update
+					let elapsed = last.elapsed().as_micros() / 100;
+					if elapsed > u32::MAX as u128 {
+						log::error!("recording exceeded maximum time between events (~119 hours), resetting.");
+						break (true, 0..0);
+					}
+					let elapsed = elapsed as u32;
 
-                    event_sum += events[*cursor].0;
-                    if elapsed < event_sum {
-                        if start_index == *cursor {
-                            return &[];
-                        }
-                        *last = Instant::now();
-                        break (false, start_index..*cursor);
-                    }
-                    *cursor += 1;
-                }
-            }
-        };
+					event_sum += events[*cursor].0;
+					if elapsed < event_sum {
+						if start_index == *cursor {
+							return &[];
+						}
+						*last = Instant::now();
+						break (false, start_index..*cursor);
+					}
+					*cursor += 1;
+				}
+			}
+		};
 
-        if reset {
-            self.reset();
-        }
+		if reset {
+			self.reset();
+		}
 
-        &self.events()[range]
-    }
-    fn events(&self) -> &[(u32, InputChanges)] {
-        match self {
-            Self::Off(events) => events,
-            Self::Playing{events, ..} => events,
-        }
-    }
-    pub fn reset(&mut self) {
-        let events = if let Self::Playing{events, ..} = self {
-            let mut tmp = Vec::new();
-            std::mem::swap(&mut tmp, events);
-            tmp
-        } else {
-            return;
-        };
-        *self = Self::Off(events);
-    }
-    pub fn is_playing(&self) -> bool {
-        if let Self::Playing{..} = self {
-            return true;
-        }
-        false
-    }
+		&self.events()[range]
+	}
+	fn events(&self) -> &[(u32, InputChanges)] {
+		match self {
+			Self::Off(events) => events,
+			Self::Playing { events, .. } => events,
+		}
+	}
+	pub fn reset(&mut self) {
+		let events = if let Self::Playing { events, .. } = self {
+			let mut tmp = Vec::new();
+			std::mem::swap(&mut tmp, events);
+			tmp
+		} else {
+			return;
+		};
+		*self = Self::Off(events);
+	}
+	pub fn is_playing(&self) -> bool {
+		if let Self::Playing { .. } = self {
+			return true;
+		}
+		false
+	}
 }
